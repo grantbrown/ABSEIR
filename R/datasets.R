27f778ca8a545b9dd375fb43fb7e91c636469817
Grant Brown <grant.brown73@gmail.com>
2015-10-05 13:47:59 -0500

    Add example data and associated documentation

---
+++ R/datasets.R
@@ -0,0 +1,42 @@
+#' Ebola Case Counts by Day - 1995 Outbreak in Kikwit
+#' 
+#' A dataset containing Ebola case counts by day between 3-6-1995 and 9-18-1995. 
+#' 
+#' 
+#' @format A data frame with 197 rows and 2 variables
+#' \describe{
+#'     \item{Date}{case index date}
+#'     \item{Count}{number of cases observed}
+#' }
+#' @references Lekone, P.E. and Finkenstadt, B.F. (2006). Statistical inference in a stochastic epidemic
+#' SEIR model with control intervention: Ebola as a case study. Biometrics 62,1170-1177
+"Kikwit1995"
+
+#' Ebola Case Count reports - 2014 and 2015 outbreak in West Africa (CDC/WHO)
+#' 
+#' A dataset containing cumulative Ebola case counts between 3-22-2014 and 3-29-2015 
+#' 
+#' 
+#' @format A data frame with 133 rows and 18 variables
+#' \describe{
+#'     \item{Date}{Case report date}
+#'     \item{Day}{Day index}
+#'     \item{Cases_Guinea}{Cumulative cases - Guinea}
+#'     \item{Cases_Liberia}{Cumulative cases - Liberia}
+#'     \item{Cases_SierraLeone}{Cumulative cases - Sierra Leone}
+#'     \item{Cases_Nigeria}{Cumulative cases - Nigeria}
+#'     \item{Cases_Senegal}{Cumulative cases - Senegal}
+#'     \item{Cases_UnitedStates}{Cumulative cases - United States}
+#'     \item{Cases_Spain}{Cumulative cases - Spain}
+#'     \item{Cases_Mali}{Cumulative cases - Mali}
+#'     \item{Deaths_Guinea}{Cumulative deaths - Guinea }
+#'     \item{Deaths_Liberia}{Cumulative deaths - Liberia }
+#'     \item{Deaths_SierraLeone}{Cumulative deaths - Sierra Leone }
+#'     \item{Deaths_Nigeria}{Cumulative deaths - Nigeria }
+#'     \item{Deaths_Senegal}{Cumulative deaths - Senegal }
+#'     \item{Deaths_UnitedStates}{Cumulative deaths - United States }
+#'     \item{Deaths_Spain}{Cumulative deaths - Spain }
+#'     \item{Deaths_Mali}{Cumulative deaths - Mali }
+#' }
+#' @source \url{https://github.com/cmrivers/ebola}
+"WestAfrica2015"
